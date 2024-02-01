6dbe7d98a1849b619bd2202a4efd44a3d591257a
Max Lell <maximilian.lell@gmail.com>
2024-02-01 11:57:09 +0000

    Added a gitignore file, added the initial draft of steps to the main.c function (pseudocode)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello, World!\n");
-  return 0;
+#define OK 0
+#define ERROR -1
+
+int main(int argc, char *argv[])
+{
+    // Parse the Search Term from the input Argument
+    if (NULL == argv[1])
+    {
+        printf("%s\n", "At least one Search Term needs to be provided");
+        return ERROR;
+    }
+
+    // Get a list of the log files which are here in the folder
+
+    // Loop through the files
+
+    //   Loop through the lines 
+
+    //     Find Strings that match the current Search term in the line
+
+    //     When a match is found 
+
+    //       when the current file name was not displayed yet
+
+    //       print out the current file name
+
+    //     else print out the current line Number and a comma
+
+
+    return OK;
 }
